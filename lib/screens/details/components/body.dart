4dea3f5880061bb939bada5999160d3d40da9948
Chinaza <ogwo.chinaza@yahoo.com>
2020-08-18 13:56:45 +0100

    added home screen with backdrop and rating

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../models/movie.dart';
+import 'backdrop_rating.dart';
+
 class Body extends StatelessWidget {
+  final Movie movie;
+
+  const Body({Key key, this.movie}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+
+    return Column(
+      children: [
+        BackdropAndRating(size: size, movie: movie),
+      ],
+    );
   }
 }
