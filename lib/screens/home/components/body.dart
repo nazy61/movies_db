6623839bfa5f8c05a5d6047373a89de45b42501d
Chinaza <ogwo.chinaza@yahoo.com>
2020-08-18 13:01:06 +0100

    done with movie card and movie carousel

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,39 +2,20 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 import 'categories.dart';
-import '../../../components/genre_card.dart';
+import 'genres.dart';
+import 'movie_carousel.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CategoryList(),
-        Genres(),
-      ],
-    );
-  }
-}
-
-class Genres extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    List<String> genres = [
-      'Action',
-      'Crime',
-      'Comedy',
-      'Drama',
-      'Horror',
-      'Animation'
-    ];
-
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
-      height: 36,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: genres.length,
-        itemBuilder: (context, index) => GenreCard(genre: genres[index]),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          CategoryList(),
+          Genres(),
+          SizedBox(height: kDefaultPadding),
+          MovieCarousel(),
+        ],
       ),
     );
   }
